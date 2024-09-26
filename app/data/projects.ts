75d79c4d2b95f34c3ec42624e918bbd68ea63c5d
SrVinicius <viniciusferreirarosario5@gmail.com>
2024-09-26 14:55:02 -0300

    just another change

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,12 +1,12 @@
 export const projects = [
-    // {
-    //   titulo: "Projeto 1",
-    //   descricao: "Um breve resumo sobre o projeto desenvolvido.",
-    //   link: "/project1",
-    // },
-    // {
-    //   titulo: "Projeto 2",
-    //   descricao: "Um breve resumo sobre o projeto desenvolvido.",
-    //   link: "/project2",
-    // },
+    {
+      titulo: "Projeto 1",
+      descricao: "Um breve resumo sobre o projeto desenvolvido.",
+      link: "/project1",
+    },
+    {
+      titulo: "Projeto 2",
+      descricao: "Um breve resumo sobre o projeto desenvolvido.",
+      link: "/project2",
+    },
 ];
